f7f515dc245746f923ff3518029b32a08b83d4ff
Derek Covey <derek.covey@gmail.com>
2018-06-27 17:24:26 -0700

    Implement simple form with redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { reduxForm, Field } from 'redux-form';
+import logo from './images/iss.png';
 import './App.css';
 
+
+let IssApiForm = props => {
+  const { handleSubmit } = props;
+
+  return <form onSubmit={handleSubmit} className="form">
+
+    <div className="field">
+      <div className="control">
+        <label className="label">First Name</label>
+        <Field className="input" name="firstName" component="input" type="text" placeholder="First Name"/>
+      </div>
+    </div>
+
+    <div className="field">
+      <div className="control">
+        <button className="button is-link">Submit</button>
+      </div>
+    </div>
+
+  </form>
+}
+
+IssApiForm = reduxForm({
+  form: 'issapi',
+})(IssApiForm);
+
 class App extends Component {
+  handleSignIn = values => {
+    console.log(values);
+  };
+
   render() {
     return (
       <div className="App">
@@ -10,9 +41,12 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+          <p className="App-intro">
+            ISS API Form
+          </p>
+          <IssApiForm onSubmit={this.handleSignIn} />
+        </div>
       </div>
     );
   }
