3d1a04f74c730052a3b7be39af14fec40188a9c9
Derek Covey <derek.covey@gmail.com>
2018-06-27 17:40:46 -0700

    Update form fields

--- src/App.js
+++ src/App.js
@@ -4,25 +4,64 @@ import logo from './images/iss.png';
 import './App.css';
 
 
+class Header extends Component {
+  render() {
+    return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">When to spot the International Space Station</h1>
+        <h3 className="App-title">Now with Redux!</h3>
+      </header>
+    );
+  }
+}
+
 let IssApiForm = props => {
   const { handleSubmit } = props;
 
-  return <form onSubmit={handleSubmit} className="form">
 
-    <div className="field">
-      <div className="control">
-        <label className="label">First Name</label>
-        <Field className="input" name="firstName" component="input" type="text" placeholder="First Name"/>
-      </div>
-    </div>
 
-    <div className="field">
-      <div className="control">
-        <button className="button is-link">Submit</button>
-      </div>
-    </div>
-
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Latitude:
+        <Field
+          name="latitude"
+          component="input"
+          type="number"
+          step="0.00001"
+          min="0"
+          max="90"
+        />
+      </label>
+      <br />
+      <label>
+        Longitude:
+        <Field
+          name="longitude"
+          component="input"
+          type="number"
+          step="0.00001"
+          min="-180"
+          max="180"
+        />
+      </label>
+      <br />
+      <label>
+        Number of passovers:
+        <Field
+          name="number"
+          component="input"
+          type="number"
+          step="1"
+          min="0"
+          max="100"
+        />
+      </label>
+      <br />
+      <button className="button">Submit</button>
   </form>
+  );
 }
 
 IssApiForm = reduxForm({
@@ -30,22 +69,19 @@ IssApiForm = reduxForm({
 })(IssApiForm);
 
 class App extends Component {
-  handleSignIn = values => {
+  handleSubmit = values => {
     console.log(values);
   };
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <div className="container">
+          <Header />
           <p className="App-intro">
             ISS API Form
           </p>
-          <IssApiForm onSubmit={this.handleSignIn} />
+          <IssApiForm onSubmit={this.handleSubmit} />
         </div>
       </div>
     );
